8f9a306aa961f38e956349b251ffa0504d621505
Marianne Tromp <mtromp@mtromp.com>
2019-01-05 14:44:26 -0500

    Make test pass

--- BowlingGame/src/main/java/BowlingGame.java
+++ BowlingGame/src/main/java/BowlingGame.java
@@ -1,10 +1,26 @@
 public class BowlingGame {
 
-    public void roll(int pins) {
+    public BowlingGame() {
+        rollNumber = 0;
+        rolls = new int[21];
+        for(int i = 0; i < rolls.length; i++) {
+            rolls[i] = 0;
+        }
+    }
 
+    public void roll(int pins) {
+        rolls[rollNumber] = pins;
+        rollNumber++;
     }
 
     public int score() {
-        return 0;
+        int total = 0;
+        for(int i = 0; i < rolls.length; i++) {
+            total += rolls[i];
+        }
+        return total;
     }
+
+    private int[] rolls;
+    private int rollNumber;
 }
