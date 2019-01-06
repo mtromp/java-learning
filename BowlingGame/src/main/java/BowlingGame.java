815bd567d55280a3edc494181cea6e35a75042f4
Marianne Tromp <mtromp@mtromp.com>
2019-01-05 19:18:15 -0500

    Refactor

--- BowlingGame/src/main/java/BowlingGame.java
+++ BowlingGame/src/main/java/BowlingGame.java
@@ -2,9 +2,13 @@ public class BowlingGame {
 
     public BowlingGame() {
         rollNumber = 0;
-        rolls = new int[21];
+        rolls = new int[maxRolls];
         for(int i = 0; i < rolls.length; i++) {
             rolls[i] = 0;
+        }
+        frames = new int[numFrames];
+        for(int i = 0; i < frames.length; i++) {
+            frames[i] = 0;
         }
     }
 
@@ -15,19 +19,8 @@ public class BowlingGame {
 
     public int score() {
         int total = 0;
-        int [] frames = new int[10];
-        for(int i = 0; i < frames.length; i++) {
-            frames[i] = 0;
-        }
 
-        int frameNumber = 0;
-        for (int i = 0; i < (rolls.length / 2); i++) {
-            frames[frameNumber] = rolls[i * 2];
-            frames[frameNumber] += rolls[(i * 2) + 1];
-
-            frameNumber++;
-        }
-        frames[9] += rolls[rolls.length - 1];
+        placeRollsInFrames();
 
         for(int i = 0; i < frames.length; i++) {
             total += frames[i];
@@ -38,6 +31,20 @@ public class BowlingGame {
         return total;
     }
 
+    private void placeRollsInFrames() {
+        int frameNumber = 0;
+        for (int i = 0; i < (rolls.length / 2); i++) {
+            frames[frameNumber] = rolls[i * 2];
+            frames[frameNumber] += rolls[(i * 2) + 1];
+
+            frameNumber++;
+        }
+        frames[9] += rolls[rolls.length - 1];
+    }
+
     private int[] rolls;
     private int rollNumber;
+    private int[] frames;
+    public final int numFrames = 10;
+    public final int maxRolls = 21;
 }
