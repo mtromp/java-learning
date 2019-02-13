1f26e23a96d6c83983e035c40b766d13957a9eb6
Marianne Tromp <mtromp@mtromp.com>
2019-02-13 15:43:38 -0500

    Test for spare calculation

--- BowlingGame/src/main/java/BowlingGame.java
+++ BowlingGame/src/main/java/BowlingGame.java
@@ -1,12 +1,35 @@
 public class BowlingGame {
 
-    private int total = 0;
+    private int[] rolls = new int[22];
+    private int rollNumber = 0;
+
     public void roll(int pins) {
-        total += pins;
+        rolls[rollNumber] = pins;
+        rollNumber++;
     }
 
     public int score() {
-        return total;
+        int theScore = 0;
+        int theRoll = 0;
+        int theFrame = 0;
+        while (theFrame < 9) {
+            if (10 == (rolls[theRoll] + rolls[theRoll + 1])) {
+                theScore += 10 + rolls[theRoll + 2];
+                theRoll += 2;
+                theFrame++;
+                continue;
+            }
+            theScore += rolls[theRoll];
+            theRoll++;
+            theScore += rolls[theRoll];
+            theRoll++;
+            theFrame++;
+        }
+        theScore += rolls[theRoll];
+        theRoll++;
+        theScore += rolls[theRoll];
+        theRoll++;
+        return theScore;
     }
 
 }
--- BowlingGame/src/test/java/BowlingGameTest.java
+++ BowlingGame/src/test/java/BowlingGameTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingGameTest {
@@ -20,6 +21,18 @@ public class BowlingGameTest {
         for (int i=0; i<20; ++i)
         {
             game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
+    @Test
+    public void rollingSpareAddsNextRollTwice() {
+        BowlingGame game = new BowlingGame();
+        game.roll(5);
+        game.roll(5);
+        game.roll(5);
+        for (int i=0; i<17; ++i)
+        {
+            game.roll(0);
         }
         assertEquals(20, game.score());
     }
