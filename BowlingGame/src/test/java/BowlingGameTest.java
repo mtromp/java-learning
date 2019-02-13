4d820ce550d57d78ed235bd26f4909f1f58d3ca1
Marianne Tromp <mtromp@mtromp.com>
2019-02-13 15:53:15 -0500

    refactor tests

--- BowlingGame/src/test/java/BowlingGameTest.java
+++ BowlingGame/src/test/java/BowlingGameTest.java
@@ -1,39 +1,39 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+    BowlingGame game;
+
+    private void multipleRolls(int numRolls, int numPins) {
+        for (int i = 0; i < numRolls; i++) {
+            game.roll(numPins);
+        }
+    }
+
+    @Before
+    public void setup() {
+        game = new BowlingGame();
+    }
+
     @Test
     public void scoreIsZeroWhenAllRollsAreZero() {
-        BowlingGame game = new BowlingGame();
-        for (int i=0; i<20; ++i)
-        {
-            game.roll(0);
-        }
+        multipleRolls(20, 0);
         assertEquals(0, game.score());
     }
     @Test
     public void rollingNonSpareNonStrikeValuesReturnsCorrectScore()
     {
-        BowlingGame game = new BowlingGame();
-        for (int i=0; i<20; ++i)
-        {
-            game.roll(1);
-        }
+        multipleRolls(20, 1);
         assertEquals(20, game.score());
     }
     @Test
     public void rollingSpareAddsNextRollTwice() {
-        BowlingGame game = new BowlingGame();
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        for (int i=0; i<17; ++i)
-        {
-            game.roll(0);
-        }
+        multipleRolls(3, 5);
+        multipleRolls(17, 0);
         assertEquals(20, game.score());
     }
 }
