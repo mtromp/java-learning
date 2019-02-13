97ac88ba707df2f97ded0271323e9208f8bfb52e
Marianne Tromp <mtromp@mtromp.com>
2019-02-13 16:00:54 -0500

    refactor code

--- BowlingGame/src/main/java/BowlingGame.java
+++ BowlingGame/src/main/java/BowlingGame.java
@@ -2,6 +2,7 @@ public class BowlingGame {
 
     private int[] rolls = new int[22];
     private int rollNumber = 0;
+    private int theScore = 0;
 
     public void roll(int pins) {
         rolls[rollNumber] = pins;
@@ -9,28 +10,31 @@ public class BowlingGame {
     }
 
     public int score() {
-        int theScore = 0;
-        int theRoll = 0;
+        rollNumber = 0;
         int theFrame = 0;
         while (theFrame < 9) {
-            if (10 == (rolls[theRoll] + rolls[theRoll + 1])) {
-                theScore += 10 + rolls[theRoll + 2];
-                theRoll += 2;
+            if (10 == (rolls[rollNumber] + rolls[rollNumber + 1])) {
+                handleSpare();
                 theFrame++;
                 continue;
             }
-            theScore += rolls[theRoll];
-            theRoll++;
-            theScore += rolls[theRoll];
-            theRoll++;
+            addRollToScore();
+            addRollToScore();
             theFrame++;
         }
-        theScore += rolls[theRoll];
-        theRoll++;
-        theScore += rolls[theRoll];
-        theRoll++;
+        addRollToScore();
+        addRollToScore();
         return theScore;
     }
 
+    private void handleSpare() {
+        theScore += 10 + rolls[rollNumber + 2];
+        rollNumber += 2;
+    }
+
+    private void addRollToScore() {
+        theScore += rolls[rollNumber];
+        rollNumber++;
+    }
 }
 
